0d73879ee6d256538d27c2e728b823fed0673b03
Eric Lee <ericstone.dev@gmail.com>
2023-02-14 17:59:44 +0800

    add new callback function with app based upload path

--- ks_shared/django/model_utils.py
+++ ks_shared/django/model_utils.py
@@ -37,6 +37,17 @@ def upload_to_without_rename(instance, filename):
         extension=filename_ext.lower(),
     )
 
+def upload_to_app_based_folder(instance, filename):
+    filename_base, filename_ext = os.path.splitext(filename)
+    return "upload/{app}/{instance}/{year}/{month}/{name}{extension}".format(
+        app=slugify(instance._meta.app_label),
+        instance=slugify(instance._meta.model_name),
+        year=datetime.now().strftime("%Y"),
+        month=datetime.now().strftime("%m"),
+        name=slugify(datetime.now().strftime("%d%H%M%S%f")),
+        extension=filename_ext.lower(),
+    )
+
 
 class BaseModelSoftDeletable(TimeStampedModel, SoftDeletableModel):
     id = models.CharField(
